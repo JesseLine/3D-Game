11cbf75dc11f7be2c16ea36909938555c910008f
JacksonDonaldson <66980702+JacksonDonaldson@users.noreply.github.com>
2022-01-07 18:34:06 -0500

    added camera movement

--- 3d-game/Assets/cameraRotation.cs
+++ 3d-game/Assets/cameraRotation.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class cameraRotation : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float sensitivity = 1;
+
     void Start()
     {
         
@@ -13,6 +16,28 @@ public class cameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float newX = transform.eulerAngles.x + Time.timeScale * -sensitivity * mouseY;
         
+        while(newX > 360)
+        {
+            newX -= 360;
+        }
+        if (newX > 180){
+            newX -= 360;
+        }
+        if (newX < -85) {
+            newX = -85;
+        }
+        else if(newX > 85){
+            print(newX);
+            newX = 85;
+        }
+        transform.eulerAngles = new Vector3(newX, transform.eulerAngles.y + Time.timeScale * sensitivity * mouseX, 0);
+
+
+
     }
 }
